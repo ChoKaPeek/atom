36f6ffab1dd7b50ef43f9ea0bb02e4f809aea6ad
Amael Tardif <amael.tardif@epita.fr>
2021-02-18 23:50:53 +0100

    widgets: subway: api v3 -> v4 and get terminus from response

--- jobs/subway.rb
+++ jobs/subway.rb
@@ -3,60 +3,63 @@ require 'open-uri'
 require 'json'
 
 # Unofficial API but it works great
-API = "https://api-ratp.pierre-grimaud.fr/v3/schedules/"
+API = "https://api-ratp.pierre-grimaud.fr/v4/schedules/"
 
-def get_next(type, path, row)
+def get_next(type, path)
     begin
         url = "#{API}#{type}/#{path}"
-        output = open(url)
+        output = URI.open(url)
         timetables = JSON.parse(output.read)
         schedules = timetables['result']['schedules']
-        if schedules.count >= row
-            if schedules[row]['message'] == "A l'approche"
-                return "0"
+        res = Array.new(schedules.count) { |e| e = [] }
+        for i in 0...schedules.count
+            if schedules[i]['message'] == "A l'approche"
+                res[i][0] = "0"
             else
-                next_one = schedules[row]['message'].scan(/\d+/).first
-                return "-" if next_one.to_s.empty?
-                next_one
+                next_one = schedules[i]['message'].scan(/\d+/).first
+                res[i][0] = next_one.to_s.empty? ? "-" : next_one
             end
-        else
-            return "?"
+            res[i][1] = schedules[i]['destination']
         end
+        return res
     rescue
-        return "?"
+        return res
     end
 end
 
 SCHEDULER.every '1m', first_in: 0 do |job|
     time = Time.new
     if 0 < time.hour && time.hour < 5
+        noct1 = get_next('noctiliens', 'n21/rue+des+jardins/A+R')
         lines = [
             {
-                name: "Noct 15",
-                icon: "noctilien/ligne15.svg",
-                in: {name: "Gabriel Peri", values: get_next('noctiliens', '15/dauphin+++anatole+france/R', 0)},
-                out: {name: "Gabriel Peri", values: get_next('noctiliens', '15/dauphin+++anatole+france/R', 1)}
-            },
-            {
-                name: "Noct 22",
-                icon: "noctilien/ligne22.svg",
-                in: {name: "Châtelet", values: get_next('noctiliens', '22/dauphin+++anatole+france/R', 0)},
-                out: {name: "Châtelet", values: get_next('noctiliens', '22/dauphin+++anatole+france/R', 1)}
+                name: "Noct. 21",
+                icon: "noctilien/ligne21.svg",
+                in1: {name: noct1[0][1], values: noct1[0][0]},
+                in2: {name: noct1[1][1], values: noct1[1][0]},
+                out1: {name: noct1[2][1], values: noct1[2][0]},
+                out2: {name: noct1[3][1], values: noct1[3][0]}
             }
         ]
     else
+        bus1 = get_next('buses', '172/centre+commercial/A+R')
+        bus2 = get_next('buses', '186/belvedere/A+R')
         lines = [
             {
-                name: "Bus 185",
-                icon: "bus/ligne185.svg",
-                in: {name: "Pt. Italie", values: get_next('bus', '185/dauphin+++anatole+france/R', 0)},
-                out: {name: "Pt. Italie", values: get_next('bus', '185/dauphin+++anatole+france/R', 1)}
+                name: "Bus 172",
+                icon: "bus/ligne172.svg",
+                in1: {name: bus1[0][1], values: bus1[0][0]},
+                in2: {name: bus1[1][1], values: bus1[1][0]},
+                out1: {name: bus1[2][1], values: bus1[2][0]},
+                out2: {name: bus1[3][1], values: bus1[3][0]}
             },
             {
-                name: "Metro 7",
-                icon: "metro/ligne7.svg",
-                in: {name: "Courneuve", values: get_next('metros', '7/villejuif+leo+lagrange/R', 0)},
-                out: {name: "Courneuve", values: get_next('metros', '7/villejuif+leo+lagrange/R', 1)}
+                name: "Bus 186",
+                icon: "bus/ligne186.svg",
+                in1: {name: bus2[0][1], values: bus2[0][0]},
+                in2: {name: bus2[1][1], values: bus2[1][0]},
+                out1: {name: bus2[2][1], values: bus2[2][0]},
+                out2: {name: bus2[3][1], values: bus2[3][0]}
             }
         ]
     end
