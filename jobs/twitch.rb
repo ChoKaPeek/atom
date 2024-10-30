65c603e1880fcfaecfe0a6b6b1028aa67bf8247d
Amael Tardif <amael.tardif@epita.fr>
2024-10-30 21:05:07 +0100

    twitch: display lives

--- jobs/twitch.rb
+++ jobs/twitch.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'erb'
 
 TWITCH='https://api.twitch.tv/helix'
-FOLLOWS=TWITCH+'/channels/followed'
+FOLLOWS=TWITCH+'/streams/followed'
 
 $access_token = ""
 
@@ -12,16 +12,26 @@ SCHEDULER.every '10m' do
 end
 
 get '/chokapeek/callback' do
-  $access_token = params['access_token']
-  send_lives()
-  redirect '/chokapeek'
+  if params['code']
+    data = post_data_form("https://id.twitch.tv/oauth2/token?client_id=#{ENV['TWITCH_CLIENT_ID']}" +
+                          "&client_secret=#{ENV['TWITCH_SECRET']}" +
+                          "&code=#{params['code']}" +
+                          "&grant_type=authorization_code" +
+                          "&redirect_uri=#{ERB::Util.url_encode('http://localhost:3030/chokapeek/callback')}"
+                         )
+    if data['access_token']
+      $access_token = data['access_token']
+      send_lives()
+      redirect '/chokapeek'
+    end
+  end
 end
 
 get '/chokapeek/twitch' do
   redirect "https://id.twitch.tv/oauth2/authorize" +
     "?client_id=#{ENV['TWITCH_CLIENT_ID']}" +
     "&redirect_uri=#{ERB::Util.url_encode('http://localhost:3030/chokapeek/callback')}" +
-    "&response_type=token" +
+    "&response_type=code" +
     "&scope=user%3Aread%3Afollows"
 end
 
@@ -36,9 +46,23 @@ end
 def send_lives()
   user_id = fetch_twitch("#{TWITCH}/users")['data'][0]['id']
 
-  data = fetch_twitch("#{FOLLOWS}?user_id=#{user_id}&first=100")
-  lives = data['data'].select { |stream| stream['type'] == 'live' }
+  streams = fetch_twitch("#{FOLLOWS}?user_id=#{user_id}&first=100")
+  lives = {}
+  ids = ""
+  streams['data'].each do |stream|
+    lives[stream['user_id']] = {
+      username: stream['user_name'],
+      title: stream['title'],
+      since: ((Time.now - Time.parse(stream['started_at'])) / 60).to_i
+    }
+
+    ids += "id=" + stream['user_id'] + "&"
+  end
+  profiles = fetch_twitch("#{TWITCH}/users?#{ids.chop}")
+  profiles['data'].each do |profile|
+    lives[profile['id']][:profile_picture] = profile['profile_image_url']
+  end
 
   send_event('twitch',
-    lives: lives)
+             lives: lives.values)
 end
