da4fd7bf57a4236822b8d9f8492b2547c225f5b1
Amael Tardif <amael.tardif@epita.fr>
2024-10-30 21:05:46 +0100

    youtube: display new vids

--- jobs/youtube.rb
+++ jobs/youtube.rb
@@ -23,17 +23,27 @@ SCHEDULER.every '30m', mutex: 'playlist0' do
 end
 
 get '/chokapeek/youtube/callback' do
-  $yt_access_token = params['access_token']
-  $playlists = get_playlists()
-  send_videos($playlists)
-  redirect '/chokapeek'
+  if params['code']
+      data = post_data_form("https://oauth2.googleapis.com/token?client_id=#{ENV['YT_CLIENT_ID']}" +
+                          "&client_secret=#{ENV['YT_SECRET']}" +
+                          "&code=#{params['code']}" +
+                          "&grant_type=authorization_code" +
+                          "&redirect_uri=#{ERB::Util.url_encode('http://localhost:3030/chokapeek/youtube/callback')}"
+                         )
+    if data['access_token']
+      $yt_access_token = data['access_token']
+      $playlists = get_playlists()
+      send_videos($playlists)
+      redirect '/chokapeek'
+    end
+  end
 end
 
 get '/chokapeek/youtube' do
   redirect "https://accounts.google.com/o/oauth2/v2/auth" +
-    "?client_id=#{ENV['YOUTUBE_CLIENT_ID']}" +
+    "?client_id=#{ENV['YT_CLIENT_ID']}" +
     "&redirect_uri=#{ERB::Util.url_encode('http://localhost:3030/chokapeek/youtube/callback')}" +
-    "&response_type=token" +
+    "&response_type=code" +
     "&scope=#{ERB::Util.url_encode('https://www.googleapis.com/auth/youtube.readonly')}"
 end
 
@@ -45,7 +55,7 @@ def fetch_yt(path)
 end
 
 def get_playlists()
-  data = fetch_yt("#{SUBS}?part=id&channelId=#{ENV['YOUTUBE_CHAN_ID']}#{SUFFIX}")
+  data = fetch_yt("#{SUBS}?part=snippet&channelId=#{ENV['YOUTUBE_CHAN_ID']}#{SUFFIX}")
   playlists = []
   loop do
     subs = []
@@ -55,13 +65,13 @@ def get_playlists()
       end
     end
 
-    chans = fetch_yt("#{CHANS}?part=contentDetails&id=#{subs.join('%2C')}#{SUFFIX}")
+    chans = fetch_yt("#{CHANS}?part=contentDetails&id=#{subs.join(',')}#{SUFFIX}")
     chans['items'].each do |j|
       playlists << j['contentDetails']['relatedPlaylists']['uploads']
     end
 
     break unless data['nextPageToken']
-    data = fetch_data("#{SUBS}?part=id&channelId=#{ENV['YOUTUBE_CHAN_ID']}#{SUFFIX}&pageToken=#{data['nextPageToken']}")
+    data = fetch_yt("#{SUBS}?part=snippet&channelId=#{ENV['YOUTUBE_CHAN_ID']}#{SUFFIX}&pageToken=#{data['nextPageToken']}")
   end
   return playlists
 end
@@ -70,7 +80,6 @@ def send_videos(playlists)
   videos = []
   true_videos = []
 
-  puts playlists
   return if playlists.empty?
 
   playlists.each do |p|
@@ -83,16 +92,16 @@ def send_videos(playlists)
   end
 
   videos.each_slice(50) do |chunk|
-    true_videos.concat(fetch_yt("#{VIDS}?part=snippet&id=#{chunk.join('%2C')}#{SUFFIX}")['items'])
+    true_videos.concat(fetch_yt("#{VIDS}?part=snippet&id=#{chunk.join(',')}#{SUFFIX}")['items'])
   end
 
-  true_videos.sort_by! { |k| k['snippet']['publishedAt'] }.reverse!.take(3)
+  true_videos = true_videos.sort_by! { |k| k['snippet']['publishedAt'] }.reverse!.take(5)
 
   list = []
   true_videos.each do |vid|
     list << {
       thumb: vid['snippet']['thumbnails']['default']['url'],
-      title: vid['snippet']['title'] 
+      title: vid['snippet']['title']
     }
   end
 
